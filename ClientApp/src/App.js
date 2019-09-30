f885dce85051c6bb10461578aa0f782b6e602794
Mars <mallorysmith6464@gmail.com>
2019-09-30 10:31:04 -0400

    added bulma, deleted some of the  template's default components

--- ClientApp/src/App.js
+++ ClientApp/src/App.js
@@ -1,31 +1,24 @@
 import React, { Component } from 'react'
 import { Route } from 'react-router'
-import { Layout } from './components/Layout'
-import { Home } from './components/Home'
-import { FetchData } from './components/FetchData'
-import { Counter } from './components/Counter'
-import Testing from './Testing'
+// import { Home } from './components/Home'
 import Nav from './components/Nav'
 import Banner from './components/Banner'
 import SubredditBody from './components/SubredditBody'
 import Community from './components/Community'
-// import 'bulma/css/bulma.css'
+import 'bulma/css/bulma.css'
+import './components/index.css'
 
 export default class App extends Component {
   static displayName = App.name
 
   render() {
     return (
-      <Layout>
-        <Route exact path="/" component={Home} />
-        {/* <Route path="/counter" component={Counter} />
-        <Route path="/fetch-data" component={FetchData} />
-        <Route path="/hello" component={Testing} /> */}
+      <>
         <Nav />
         <Banner />
         <SubredditBody />
         <Community />
-      </Layout>
+      </>
     )
   }
 }
