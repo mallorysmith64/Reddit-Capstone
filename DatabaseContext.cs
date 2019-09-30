a5b3c9d41e4d2a1c649b499cf08320e555c97d6a
Mars <mallorysmith6464@gmail.com>
2019-09-30 11:21:13 -0400

    fixed package.json files by removing unnecessary package.json files and then doing cd into clientapp and doing npm install bulma

--- DatabaseContext.cs
+++ DatabaseContext.cs
@@ -29,7 +29,7 @@ namespace reddit_capstone
       if (!optionsBuilder.IsConfigured)
       {
         var envConn = Environment.GetEnvironmentVariable("DATABASE_URL");
-#warning Be sure to update to your correct connection string to the point to the correct database
+        // #warning Be sure to update to your correct connection string to the point to the correct database
         var conn = "server=localhost;database=redditDB";
         if (envConn != null)
         {
