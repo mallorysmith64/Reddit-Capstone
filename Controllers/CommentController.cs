32252ef34d1d8693b2bffdb2a5ce03c0b5219379
Mars <mallorysmith6464@gmail.com>
2019-09-30 10:58:06 -0400

    added post and comment controller

---
+++ Controllers/CommentController.cs
@@ -0,0 +1,96 @@
+using System.Collections.Generic;
+using System.Linq;
+//reference databasecontext
+using reddit_capstone;
+using StackOverFlow.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace StackOverFlow.Controllers
+{
+  [Route("api/[controller]")]
+  [ApiController]
+  public class CommentController : ControllerBase
+  {
+    //dependency injection
+    private DatabaseContext context;
+
+    public CommentController(DatabaseContext _context)
+    {
+      this.context = _context;
+    }
+
+    //this does not work yet, error 500 on post request
+    //make an answer/comment
+    [HttpPost]
+    public ActionResult<Comment> CreateEntry([FromBody]Comment entry)
+    {
+      context.Comments.Add(entry);
+      context.SaveChanges();
+      return entry;
+    }
+
+    //get all answers/comments
+    [HttpGet]
+    public ActionResult<IEnumerable<Comment>> GetAllComments()
+    {
+      var comments = context.Comments.OrderByDescending(comment => comment.ID);
+      return comments.ToList();
+    }
+
+    //get an answer/comment
+    [HttpGet("{id}")]
+    public ActionResult GetPost(int id)
+    {
+      var comment = context.Comments.FirstOrDefault(o => o.ID == id);
+      if (comment == null)
+      {
+        return NotFound();
+      }
+      else
+      {
+        return Ok(comment);
+      }
+    }
+
+    //does not work yet; error 400 on put request
+    //update answer/comment
+    [HttpPut("{id}")]
+    public ActionResult<Comment> Update(int id, [FromBody]Comment newDetails)
+    {
+      if (id != newDetails.ID)
+      {
+        return BadRequest();
+      }
+      context.Comments.Update(newDetails);
+      context.SaveChanges();
+      return newDetails;
+    }
+
+    [HttpDelete("{id}")]
+    public ActionResult DeleteBlog(int id)
+    {
+      var comment = context.Comments.FirstOrDefault(f => f.ID == id);
+      //add response message later within the return
+      return Ok();
+    }
+
+    // [HttpPost("{postId}/comments")]
+    // public ActionResult<Comment> CreateComment(int postId, [FromBody]Comment comment)
+    // {
+    //   // check if the blog exists
+    //   var post = context.Posts.FirstOrDefault(f => f.ID == postId);
+    //   if (post == null)
+    //   {
+    //     return NotFound();
+    //   }
+    //   else
+    //   {
+    //     comment.PostID = postId;
+    //     context.Comments.Add(comment);
+    //     context.SaveChanges();
+    //     return Ok();
+    //   }
+    // }
+  }
+}
+
---
+++ Controllers/PostController.cs
@@ -0,0 +1,96 @@
+using System.Collections.Generic;
+using System.Linq;
+//reference databasecontext
+using reddit_capstone;
+using StackOverFlow.Models;
+using Microsoft.AspNetCore.Mvc;
+// using Microsoft.EntityFrameworkCore;
+// using StackOverFlow.ViewModels;
+
+namespace StackOverFlow.Controllers
+{
+  [Route("api/[controller]")]
+  [ApiController]
+  public class PostController : ControllerBase
+  {
+    //dependency injection
+    private DatabaseContext context;
+
+    public PostController(DatabaseContext _context)
+    {
+      this.context = _context;
+    }
+    //the only requests that should be [FromBody] is post and put, get and delete are NOT [FromBody]
+
+    //make a question
+    [HttpPost]
+    public ActionResult<Post> CreateEntry([FromBody]Post entry)
+    {
+      context.Posts.Add(entry);
+      context.SaveChanges();
+      return entry;
+    }
+
+    //get all questions
+    [HttpGet]
+    public ActionResult<IEnumerable<Post>> GetAllPosts()
+    {
+      var posts = context.Posts.OrderByDescending(post => post.TimePassed);
+      return posts.ToList();
+    }
+
+    //get a question
+    [HttpGet("{id}")]
+    public ActionResult GetPost(int id)
+    {
+      var post = context.Posts.FirstOrDefault(o => o.ID == id);
+      if (post == null)
+      {
+        return NotFound();
+      }
+      else
+      {
+        return Ok(post);
+      }
+    }
+
+    //update question
+    [HttpPut("{id}")]
+    public ActionResult<Post> Update(int id, [FromBody]Post newDetails)
+    {
+      if (id != newDetails.ID)
+      {
+        return BadRequest();
+      }
+      context.Posts.Update(newDetails);
+      context.SaveChanges();
+      return newDetails;
+    }
+
+    // [HttpDelete("{id}")]
+    // public ActionResult DeleteBlog(int id)
+    // {
+    //   var post = context.Posts.FirstOrDefault(f => f.ID == id);
+    //   return Ok(new DeleteResponse { Post = post });
+    // }
+
+    [HttpPost("{postId}/comments")]
+    public ActionResult<Comment> CreateComment(int postId, [FromBody]Comment comment)
+    {
+      // check if the blog exists
+      var post = context.Posts.FirstOrDefault(f => f.ID == postId);
+      if (post == null)
+      {
+        return NotFound();
+      }
+      else
+      {
+        comment.PostID = postId;
+        context.Comments.Add(comment);
+        context.SaveChanges();
+        return Ok();
+      }
+    }
+  }
+}
+
