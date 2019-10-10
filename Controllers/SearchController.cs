72ef533d47dff1b45e08ae286180f1c91ac08145
Mars <mallorysmith6464@gmail.com>
2019-10-10 09:35:15 -0400

    search controller responds on swagger

--- Controllers/SearchController.cs
+++ Controllers/SearchController.cs
@@ -20,15 +20,18 @@ namespace StackOverFlow.Controllers
 
     //get search terms first
     // [HttpGet("search/{searchTerm}")]
-    [HttpGet("posts")]
     //get a list of posts based off a string instead of a number/the id
+    [HttpGet("posts")]
     public async Task<ActionResult> SearchPosts([FromQuery] string query)
     {
       query = query.ToLower();
       var results = await context.Posts.Where(post =>
+      //convert strings toLower()
       post.PostedBy.ToLower().Contains(query) ||
-      post.Title.ToString().Contains(query) ||
-      post.Content.ToString().Contains(query) ||
+      post.Title.ToLower().Contains(query) ||
+      post.Content.ToLower().Contains(query) ||
+
+      //dates should be toString()
       post.TimePassed.ToString().Contains(query)
       ).ToListAsync();
       return Ok(new { SearchingFor = query, results = results });
